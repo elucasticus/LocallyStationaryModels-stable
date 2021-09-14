823c930cdc1c6750dd60440864e272f68211a8f4
giacomodecarlo <80590505+giacomodecarlo@users.noreply.github.com>
2021-09-14 13:51:57 +0200

    Add files via upload

---
+++ src/RcppEigenInterface.cpp
@@ -0,0 +1,46 @@
+// -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; indent-tabs-mode: nil; -*-
+
+#include <RcppEigen.h>
+#include "crippadecarlo.hpp"
+using namespace cd;
+using namespace LBFGSpp;
+using namespace std::chrono;
+
+// [[Rcpp::depends(RcppEigen)]]
+
+// [[Rcpp::export]]
+Rcpp::List fullmodel(const Eigen::VectorXd &y, const Eigen::MatrixXd &d, const double& epsilon, const double& h) {
+    auto start = high_resolution_clock::now();
+    
+    
+    //d = stampante::caricamatrice("d.csv");
+   //y = stampante::caricavettore("y.csv");
+   
+    vectorind positions({14,17,37,87,107,117,23,34});
+    
+    //matrixptr *dd = &d;
+    matrixptr dd = std::make_shared<matrix>(d);
+    vectorptr yy = std::make_shared<vector>(y);
+    
+    //vectorptr *yy = &y;
+    
+    crippadecarlo CD(dd, yy, epsilon, h, positions);
+    vector newpos = dd->row(0);
+    double delta = CD.get_delta();
+    double epsilon_ = CD.get_epsilon();
+    
+    
+    return Rcpp::List::create(Rcpp::Named("positions")=positions,
+                              Rcpp::Named("values")=y,
+                              Rcpp::Named("ypredicted")=CD.predict_y(newpos),
+                              Rcpp::Named("delta")=delta,
+                              Rcpp::Named("delta")=epsilon_);
+    
+    
+    
+ 
+   //auto stop = high_resolution_clock::now();
+    //auto duration = duration_cast<milliseconds>(stop - start);
+    
+}
+
