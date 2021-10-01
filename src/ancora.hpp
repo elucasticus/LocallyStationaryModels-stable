df20d28ad4f0ea88a9927234bf385434cda40a55
elucasticus <luca7.crippa@mail.polimi.it>
2021-10-01 15:01:51 +0200

    modificata ancora

--- src/ancora.hpp
+++ src/ancora.hpp
@@ -20,11 +20,11 @@ private:
     cd::vector cubotti() 
     {
         unsigned int n = data->rows();
-        center_x = (data->col(0)).minCoeff();
-        center_y = (data->col(1)).minCoeff();
+        center_x = (data->col(0)).minCoeff()*0.99;
+        center_y = (data->col(1)).minCoeff()*0.99;
 
-        larghezza = (data->col(0)).maxCoeff() - center_x;
-        altezza = (data->col(1)).maxCoeff() - center_y;
+        larghezza = (data->col(0)).maxCoeff()*1.01 - center_x;
+        altezza = (data->col(1)).maxCoeff()*1.01 - center_y;
 
         larghezza_cubo = larghezza/n_cubotti;
         altezza_cubo = altezza/n_cubotti;
