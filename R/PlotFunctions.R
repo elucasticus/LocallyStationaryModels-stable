9e8ff60138ccd8e9ef943e4cdf9d42d18e527d71
elucasticus <luca7.crippa@mail.polimi.it>
2021-10-10 22:07:23 +0200

    sostituite latitudine e longitudine con x e y

--- R/PlotFunctions.R
+++ R/PlotFunctions.R
@@ -1,25 +1,25 @@
 plot.lsm<-function(model, a, y, d)
 {
   aa<-as.data.frame(a$anchorpoints)
-  colnames(aa)<-c("latitude","longitude")
+  colnames(aa)<-c("X","Y")
   s<-model$solutions
   s<-as.data.frame(s)
   colnames(s)<-c("lambda1", "lambda2", "phi", "sigma")
   g<-cbind(aa,s)
   
-  newpoints <- data.frame(latitude = double(), longitude = double())
+  newpoints <- data.frame(X = double(), Y = double())
   for (i in 1:dim(g)[1])
   {
-    #newpoints<-rbind(newpoints, c(g$latitude[i]+a$width/2, g$longitude[i]))
-    #newpoints<-rbind(newpoints, c(g$latitude[i]-a$width/2, g$longitude[i]))
-    #newpoints<-rbind(newpoints, c(g$latitude[i], g$longitude[i]+a$height/2))
-    #newpoints<-rbind(newpoints, c(g$latitude[i], g$longitude[i]-a$height/2))
-    newpoints<-rbind(newpoints, c(g$latitude[i]+a$width/sqrt(2), g$longitude[i]+a$height/sqrt(2)))
-    newpoints<-rbind(newpoints, c(g$latitude[i]+a$width/sqrt(2), g$longitude[i]-a$height/sqrt(2)))
-    newpoints<-rbind(newpoints, c(g$latitude[i]-a$width/sqrt(2), g$longitude[i]+a$height/sqrt(2)))
-    newpoints<-rbind(newpoints, c(g$latitude[i]-a$width/sqrt(2), g$longitude[i]-a$height/sqrt(2)))
+    #newpoints<-rbind(newpoints, c(g$X[i]+a$width/2, g$Y[i]))
+    #newpoints<-rbind(newpoints, c(g$X[i]-a$width/2, g$Y[i]))
+    #newpoints<-rbind(newpoints, c(g$X[i], g$Y[i]+a$height/2))
+    #newpoints<-rbind(newpoints, c(g$X[i], g$Y[i]-a$height/2))
+    newpoints<-rbind(newpoints, c(g$X[i]+a$width/sqrt(2), g$Y[i]+a$height/sqrt(2)))
+    newpoints<-rbind(newpoints, c(g$X[i]+a$width/sqrt(2), g$Y[i]-a$height/sqrt(2)))
+    newpoints<-rbind(newpoints, c(g$X[i]-a$width/sqrt(2), g$Y[i]+a$height/sqrt(2)))
+    newpoints<-rbind(newpoints, c(g$X[i]-a$width/sqrt(2), g$Y[i]-a$height/sqrt(2)))
   }
-  colnames(newpoints)<-c("latitude","longitude")
+  colnames(newpoints)<-c("X","Y")
   
   parameters<-smoothing(model$solutions,a$anchorpoints,model$delta,as.matrix(newpoints))
   parameters<-as.data.frame(parameters)
@@ -35,26 +35,26 @@ plot.lsm<-function(model, a, y, d)
   #print(p)
   
   par(ask=TRUE)
-  p <- ggplot(dd, aes(x=V1, y=V2, size=y)) + geom_point()
+  p <- ggplot(dd, aes(x=V1, y=V2, size=y)) + geom_point() + labs(x="X", y="Y")
   print(p)
   
   
   ###ELLISSI
-  p <- ggplot(g, aes(x=latitude, y=longitude)) + geom_ellipse(aes(x0 = latitude, y0 = longitude, a = lambda1, b = lambda2, angle = phi), data = g) + coord_fixed()
+  p <- ggplot(g, aes(x=X, y=Y)) + geom_ellipse(aes(x0 = X, y0 = Y, a = lambda1, b = lambda2, angle = phi), data = g) + coord_fixed()
   print(p)
   
   ###PARAMETERS
-  p1 <- ggplot(allpoints, aes(x=latitude, y=longitude, color=lambda1)) + geom_point() + scale_color_gradientn(colours = rainbow(5)) + coord_fixed()
-  p2 <- ggplot(allpoints, aes(x=latitude, y=longitude, color=lambda2)) + geom_point() + scale_color_gradientn(colours = rainbow(5)) + coord_fixed()
-  p3 <- ggplot(allpoints, aes(x=latitude, y=longitude, color=phi)) + geom_point() + scale_color_gradientn(colours = rainbow(5)) + coord_fixed()
-  p4 <- ggplot(allpoints, aes(x=latitude, y=longitude, color=sigma)) + geom_point() + scale_color_gradientn(colours = rainbow(5)) + coord_fixed()
+  p1 <- ggplot(allpoints, aes(x=X, y=Y, color=lambda1)) + geom_point() + scale_color_gradientn(colours = rainbow(5)) + coord_fixed()
+  p2 <- ggplot(allpoints, aes(x=X, y=Y, color=lambda2)) + geom_point() + scale_color_gradientn(colours = rainbow(5)) + coord_fixed()
+  p3 <- ggplot(allpoints, aes(x=X, y=Y, color=phi)) + geom_point() + scale_color_gradientn(colours = rainbow(5)) + coord_fixed()
+  p4 <- ggplot(allpoints, aes(x=X, y=Y, color=sigma)) + geom_point() + scale_color_gradientn(colours = rainbow(5)) + coord_fixed()
   print(plot_grid(p1,p2,p3,p4,labels="AUTO"))
   
   
   ###FUNCTION VALUES
   predictedvalues<-predikt(y,d,model$anchorpoints,model$epsilon,model$delta,model$solutions,as.matrix(allpoints)[,1:2])
-  means <- ggplot(allpoints, aes(x=latitude, y=longitude, color=predictedvalues$predictedmean)) + geom_point() + scale_color_gradientn(colours = rainbow(5)) + coord_fixed()
-  ys <- ggplot(allpoints, aes(x=latitude, y=longitude, color=predictedvalues$ypredicted)) + geom_point() + scale_color_gradientn(colours = rainbow(5)) + coord_fixed()
+  means <- ggplot(allpoints, aes(x=X, y=Y, color=predictedvalues$predictedmean)) + geom_point() + scale_color_gradientn(colours = rainbow(5)) + coord_fixed()
+  ys <- ggplot(allpoints, aes(x=X, y=Y, color=predictedvalues$ypredicted)) + geom_point() + scale_color_gradientn(colours = rainbow(5)) + coord_fixed()
   means<-means+labs(color="mean")
   ys<-ys+labs(color="y")
   print(plot_grid(means, ys,labels="AUTO"))
