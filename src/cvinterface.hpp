2b3a2ea6172729c3fea496cbefd11b4d197b83fe
elucasticus <luca7.crippa@mail.polimi.it>
2021-10-27 13:45:44 +0200

    Aggiunti file spariti

---
+++ src/cvinterface.hpp
@@ -0,0 +1,59 @@
+/// Copyright (C) Luca Crippa <luca7.crippa@mail.polimi.it>
+/// Copyright (C) Giacomo De Carlo <giacomo.decarlo@mail.polimi.it>
+/// Under MIT license
+
+#ifndef LOCALLY_STATIONARY_MODELS_CVINTERFACE
+#define LOCALLY_STATIONARY_MODELS_CVINTERFACE
+
+#include "samplevar.hpp"
+#include "variogramfit.hpp"
+#include <iostream>
+#include <chrono>
+#include "smooth.hpp"
+#include "kriging.hpp"
+
+class cvinterface
+{
+private:
+    cd::matrixptr d;
+    cd::vectorptr y;
+    cd::matrixptr anchorpoints;
+    double epsilon_ottimale;
+    double delta_ottimale;
+
+    predictor predictor_;
+
+    cd::matrixptr solutions = nullptr;
+    cd::matrixptr empvar = nullptr;
+    cd::matrixptr kernelmatrix = nullptr;
+    cd::matrixIptr gridptr = nullptr;
+
+public:
+    cvinterface(const cd::matrixptr &d_, const cd::vectorptr &y_, cd::matrixptr anchorpoints_, const cd::vector &parameters, const double epsilon, const unsigned int n_angles, const unsigned int n_intervals,
+        const std::string &kernel_id, const std::string &variogram_id);
+    cvinterface(const cd::matrixptr &d_, const cd::vectorptr &y_, cd::matrixptr anchorpoints_, const cd::vector &parameters, const double min_epsilon, const double max_epsilon, const unsigned int& nepsilons,  const unsigned int n_angles, 
+        const unsigned int n_intervals, const std::string &kernel_id, const std::string &variogram_id);
+    cvinterface(const cd::matrixptr &d_, const cd::vectorptr &y_, cd::matrixptr anchorpoints_, const double epsilon, const double delta, const cd::matrixptr &solutions_, const std::string &variogram_id);
+    
+    template<class Input, class Output>
+    Output predict_mean(const Input &pos) const{return predictor_.predict_mean<Input, Output>(pos);};
+
+    template<class Input, class Output>
+    Output predict_y(const Input &pos) const{return predictor_.predict_y<Input, Output>(pos);};
+
+    double get_epsilon() const;
+    double get_delta() const;
+    const cd::matrixptr get_solutions() const;
+    const cd::matrixptr get_empiricvariogram() const;
+    const cd::matrixptr get_kernel() const;
+    const cd::matrixIptr get_grid() const;
+};
+
+
+
+
+
+
+
+
+#endif //LOCALLY_STATIONARY_MODELS_CVINTERFACE
