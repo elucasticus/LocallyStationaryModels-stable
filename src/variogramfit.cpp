2f93d8fe0087b42468bc26bce0f1928eb7369860
elucasticus <luca7.crippa@mail.polimi.it>
2021-11-07 16:07:55 +0100

    sistemata parentesi

--- src/variogramfit.cpp
+++ src/variogramfit.cpp
@@ -49,7 +49,7 @@ cd::scalar funzionedaottimizzare::operator() (const cd::vector &params, vector &
     }
     
     vector empiricgamma = empiricvariogram->col(x0);
-    return w.dot((truegamma - empiricgamma)).cwiseProduct(truegamma - empiricgamma);
+    return w.dot((truegamma - empiricgamma).cwiseProduct(truegamma - empiricgamma));
 }
 
 funzionedaottimizzare::funzionedaottimizzare(const cd::matrixptr empiricvariogram_, const cd::matrixptr squaredweights_, const cd::vectorptr x_, const cd::vectorptr y_, unsigned int x0_, 
