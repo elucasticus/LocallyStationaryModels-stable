f52fa14a705d63becef264ed1b7de43910c54ab4
giacomodecarlo <giacomo.decarlo@mail.polimi.it>
2021-11-30 09:51:25 +0100

    modificato test con esempio svizzera

--- testserio.R
+++ testserio.R
@@ -19,11 +19,11 @@ solu <- findsolutions.lsm(vario, "exponential", c(200,200,0.01,100))
 solu
 # Plot of the solutions
 x11()
-mypoints<-plot.lsm(model = solu, a = a, n_points = 10, points_arrangement = "random", bool = TRUE)
+mypoints<-plot.lsm(model = solu, a = a, z = y, d = d, n_points = 10, points_arrangement = "random", bool = TRUE)
 
 # Kriging on the original data
 x11()
-previsions <- predict.lsm(solu, d)
+previsions <- predict.lsm(solu, d, y, d)
 max(previsions$zpredicted - y)
 
 # Test the performace of our model via cross-validation
@@ -38,21 +38,22 @@ d <- cbind(V1, V2)
 
 
 # Find anchorpoints
-a <- find_anchorpoints.lsm(d,8)
+a <- find_anchorpoints.lsm(d,12)
 # Build the empiric variogram
-vario <- variogram.lsm(y,d,a$anchorpoints,46*2,6,15,"gaussian")
+vario <- variogram.lsm(y,d,a$anchorpoints,46,6,15,"gaussian")
 # Find the solutions
-solu <- findsolutions.lsm(vario, "exponential", c(20,100,0.51,100),upper.bound = c(Inf,Inf,pi/2,250))
+solu <- findsolutions.lsm(vario, "exponential", c(30,30,0.5,100),upper.bound = c(46,46,pi/2,250))
 solu
-plotvario(6,15,vario$empiricvariogram,34,46*2)
+plotvario(vario,3)
 # Plot of the solutions
 x11()
-mypoints<-plot.lsm(model = solu, a = a, z = y, d = d, n_points = 10, points_arrangement = "random")
+mypoints<-plot.lsm(model = solu, a = a, z = y, d = d, n_points = 5, points_arrangement = "straight")
 
 # Kriging on the original data
 x11()
 previsions <- predict.lsm(solu, d, y, d)
 max(previsions$zpredicted - y)
+
 
 
 
